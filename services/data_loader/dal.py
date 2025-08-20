b9773145bba43263316cca65d8304827b8f7f087
david horovitz <dovidho@gmail.com>
2025-08-20 11:49:12 +0300

    dal2

--- services/data_loader/dal.py
+++ services/data_loader/dal.py
@@ -10,30 +10,33 @@ class DataLoader:
         self.MONGO_PORT = os.getenv("MONGO_PORT", "27017")
         self.MONGO_DB = os.getenv("MONGO_DB", "enemy_soldiers")
 
-        url = f"mongodb://{self.MONGO_USER}:{self.MONGO_PASS}@{self.MONGO_HOST}:{self.MONGO_PORT}/{self.MONGO_DB}"
-        - MONGO_INITDB_ROOT_USERNAME =$MONGO_INITDB_ROOT_USERNAME
-        - MONGO_INITDB_ROOT_PASSWORD =$MONGO_INITDB_ROOT_PASSWORD
-        - APP_USER =$APP_USER
-        - APP_PWD =$APP_PWD
-        - DB_NAME =$DB_NAME
-        - DB_COLLECTION_NAME =$DB_COLLECTION_NAME
-        - MONGO_HOSTNAME =$MONGO_HOSTNAME
+        self.url = f"mongodb://{self.MONGO_USER}:{self.MONGO_PASS}@{self.MONGO_HOST}:{self.MONGO_PORT}/{self.MONGO_DB}"
+
 
     def get_all_data(self):
-        myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-        mydb = myclient["mydatabase"]
+        # myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+        myclient = pymongo.MongoClient(self.url)
+        mydb = myclient[self.MONGO_DB]
+        mycol = mydb["enemy_soldiers"]
+        return list(mycol.find())
 
     def insert_data(self,ID,first_name,last_name,phone_number,rank):
-        myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-        mydb = myclient["mydatabase"]
+        myclient = pymongo.MongoClient(self.url)
+        mydb = myclient[self.MONGO_DB]
+        mycol = mydb["enemy_soldiers"]
         return {"status insert data:ok"}
 
-    def update_data(self,ID,param):
-        myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-        mydb = myclient["mydatabase"]
-        return {"status update data:ok"}
+
+    def update_data(self, ID, param):
+        myclient = pymongo.MongoClient(self.url)
+        mydb = myclient[self.MONGO_DB]
+        mycol = mydb["enemy_soldiers"]
+        mycol.update_one({"ID": ID}, {"$set": param})
+        return {"status update data: ok"}
 
     def delete_data(self,ID):
-        myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-        mydb = myclient["mydatabase"]
+        myclient = pymongo.MongoClient(self.url)
+        mydb = myclient[self.MONGO_DB]
+        mycol = mydb["enemy_soldiers"]
+        mycol.delete_one({"ID": ID})
         return {"status delete data:ok"}
